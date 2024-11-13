644b6d593b49f5195b863d6e78d8743dfb6d795d
Matteo Bossolini <matteobossolini@Mac.fritz.box>
2024-11-13 23:16:43 +0100

    Aggiornamento metodi supportati

---
+++ first_prompt.py
@@ -0,0 +1,26 @@
+import anthropic
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = anthropic.Anthropic()
+
+message = client.messages.create(
+    model="claude-3-5-sonnet-20241022",
+    max_tokens=1000,
+    temperature=0,
+    system="you are a world class poet. write only short answer",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "Why is the ocean salty?"
+                }
+            ]
+        }
+    ]
+)
+print(message.content)
